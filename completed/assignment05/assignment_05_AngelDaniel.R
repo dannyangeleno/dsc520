6432c4b0030eb2c7e19d22d1a38591149265ce01
dannyangeleno <70503776+dannyangeleno@users.noreply.github.com>
2021-01-17 18:32:43 -0800

    Upload assignment 5 and console output .txt

---
+++ completed/assignment05/assignment_05_AngelDaniel.R
@@ -0,0 +1,26 @@
+# Assignment: ASSIGNMENT 5
+# Name: Angel, Daniel
+# Date: 2021-01-17
+
+
+## Set the working directory to the root of your DSC 520 directory
+setwd("C:/Users/Danny/Documents")
+
+## Load the `data/r4ds/heights.csv` to
+heights_df <- read.csv("data/r4ds/heights.csv")
+
+## Using `cor()` compute correclation coefficients for
+## height vs. earn
+cor(x = heights_df$height, y= heights_df$earn)
+### age vs. earn
+cor(x = heights_df$age, y= heights_df$earn)
+### ed vs. earn
+cor(x = heights_df$ed, y= heights_df$earn)
+
+## Spurious correlation
+## The following is data on US spending on science, space, and technology in millions of today's dollars
+## and Suicides by hanging strangulation and suffocation for the years 1999 to 2009
+## Compute the correlation between these variables
+tech_spending <- c(18079, 18594, 19753, 20734, 20831, 23029, 23597, 23584, 25525, 27731, 29449)
+suicides <- c(5427, 5688, 6198, 6462, 6635, 7336, 7248, 7491, 8161, 8578, 9000)
+cor(x = tech_spending, y = suicides )
