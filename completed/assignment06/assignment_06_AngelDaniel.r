6f58afc65ef5d7b3875b3db353c7a68a40f87e0b
dannyangeleno <70503776+dannyangeleno@users.noreply.github.com>
2021-01-31 21:09:54 -0800

    Fix upload

--- completed/assignment06/assignment_06_AngelDaniel.r
+++ completed/assignment06/assignment_06_AngelDaniel.r
@@ -1,9 +1,9 @@
 # Assignment: ASSIGNMENT 6
-# Name: Lastname, Firstname
-# Date: 2010-02-14
+# Name: Angel, Daniel
+# Date: 2020-01-24
 
 ## Set the working directory to the root of your DSC 520 directory
-setwd("/home/jdoe/Workspaces/dsc520")
+setwd("C:/Users/Danny/Documents")
 
 ## Load the `data/r4ds/heights.csv` to
 heights_df <- read.csv("data/r4ds/heights.csv")
@@ -12,53 +12,66 @@ heights_df <- read.csv("data/r4ds/heights.csv")
 library(ggplot2)
 
 ## Fit a linear model using the `age` variable as the predictor and `earn` as the outcome
-age_lm <-  ___
+age_lm <-  lm(earn ~ age, data = heights_df)
 
 ## View the summary of your model using `summary()`
-___
+summary(age_lm)
 
 ## Creating predictions using `predict()`
-age_predict_df <- data.frame(earn = predict(___, ___), age=___)
+age_predict_df <- data.frame(earn = predict(age_lm, heights_df), age= heights_df$age)
 
 ## Plot the predictions against the original data
-ggplot(data = ___, aes(y = ___, x = ___)) +
-  geom_point(color='blue') +
-  geom_line(color='red',data = ___, aes(y=___, x=___))
+  ggplot(data = heights_df, aes(y = earn, x = age)) + 
+    geom_point(color='blue')  + 
+    geom_line(color='red',data = age_predict_df, aes(y= earn, x= age))
 
 mean_earn <- mean(heights_df$earn)
+mean_earn
 ## Corrected Sum of Squares Total
 sst <- sum((mean_earn - heights_df$earn)^2)
+sst
 ## Corrected Sum of Squares for Model
 ssm <- sum((mean_earn - age_predict_df$earn)^2)
+ssm
 ## Residuals
 residuals <- heights_df$earn - age_predict_df$earn
 ## Sum of Squares for Error
 sse <- sum(residuals^2)
+sse
 ## R Squared R^2 = SSM\SST
-r_squared <- ___
-
+r_squared <- ssm/sst
+r_squared
 ## Number of observations
-n <- ___
+n <- nrow(heights_df)
+n
 ## Number of regression parameters
 p <- 2
+p
 ## Corrected Degrees of Freedom for Model (p-1)
-dfm <- ___
+dfm <- p-1
+dfm
 ## Degrees of Freedom for Error (n-p)
-dfe <- ___
+dfe <- n-p
+dfe
 ## Corrected Degrees of Freedom Total:   DFT = n - 1
-dft <- ___
-
+dft <- n-1
+dft
 ## Mean of Squares for Model:   MSM = SSM / DFM
-msm <- ___
+msm <- ssm/dfm
+msm
 ## Mean of Squares for Error:   MSE = SSE / DFE
-mse <- ___
+mse <- sse/dfe
+mse
 ## Mean of Squares Total:   MST = SST / DFT
-mst <- ___
+mst <- sst/dft
+mst
 ## F Statistic F = MSM/MSE
-f_score <- ___
-
+f_score <- msm/mse
+f_score
 ## Adjusted R Squared R2 = 1 - (1 - R2)(n - 1) / (n - p)
-adjusted_r_squared <- ___
-
+adjusted_r_squared <- 1-((1-r_squared)*(dft)/dfe)
+adjusted_r_squared
 ## Calculate the p-value from the F distribution
 p_value <- pf(f_score, dfm, dft, lower.tail=F)
+p_value
+
